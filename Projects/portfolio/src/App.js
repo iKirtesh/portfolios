4f73abca257314c7d93e312689bf94e7f3697f40
Kirtesh <kirteshadmute@gmail.com>
2024-02-25 17:59:49 +0530

    My First Commit

---
+++ Projects/portfolio/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className="App overflow-hidden">
+
+        <header className="App-header text-3xl font-bold">
+          <h1 className="text-5xl font-bold">
+            I'm Kirtesh Admute
+          </h1>
+          Coming Soon...
+        </header>
+      </div>
+  );
+}
+
+export default App;
